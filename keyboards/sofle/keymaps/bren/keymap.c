42f69300ab56c60c5ec9195eaa3de9c55a5879f1
Brenainn Moushall <bmoush@gmail.com>
2021-09-13 16:52:06 +1000

    Swap ctrl and shift

--- keyboards/sofle/keymaps/bren/keymap.c
+++ keyboards/sofle/keymaps/bren/keymap.c
@@ -18,29 +18,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_INSERT, \
       KC_HOME,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_END, \
       KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOTE, \
-      KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
-                      KC_LALT, KC_LGUI, KC_LCTRL, KC_SYM, KC_SPC,      KC_ENT,  KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
+      KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
+                      KC_LALT, KC_LGUI, KC_SYM, KC_LSFT, KC_SPC,      KC_ENT,  KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
     ),
     [_GAME] = LAYOUT( \
       KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_INSERT, \
       KC_I,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_END, \
       KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOTE, \
-      KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
-                    KC_LALT, KC_LGUI, KC_LCTRL, KC_M, KC_SPC,                   KC_ENT, KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
+      KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
+                    KC_LALT, KC_LGUI, KC_M, KC_LSFT, KC_SPC,                   KC_ENT, KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
     ),
     [_SYM] = LAYOUT( \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
       XXXXXXX, XXXXXXX, KC_RPRN, KC_RBRC, KC_RCBR,  XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
       XXXXXXX, KC_UNDS, KC_LPRN, KC_LBRC, KC_LCBR, KC_EQUAL,                             KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,\
-      KC_LSFT, KC_MINUS, XXXXXXX, KC_BSLS, KC_PIPE, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-      	     KC_LALT, KC_LGUI, KC_LCTRL, _______, KC_SPC,                            KC_ENT, KC_BSPC, XXXXXXX, KC_PGDOWN, KC_PGUP \
+      KC_LCTRL, KC_MINUS, XXXXXXX, KC_BSLS, KC_PIPE, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      	     KC_LALT, KC_LGUI, _______, KC_LSFT, KC_SPC,                            KC_ENT, KC_BSPC, XXXXXXX, KC_PGDOWN, KC_PGUP \
     ),
     [_FN] = LAYOUT( \
       KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                 KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
       XXXXXXX, KC_PWR, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GAME,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-              KC_LALT, KC_LGUI, KC_LCTRL, XXXXXXX, KC_SPC,                       KC_ENT, KC_BSPC, _______, KC_PGDOWN, KC_PGUP \
+      KC_LCTRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+              KC_LALT, KC_LGUI, XXXXXXX, KC_LSFT, KC_SPC,                       KC_ENT, KC_BSPC, _______, KC_PGDOWN, KC_PGUP \
     )
 };
 
@@ -94,7 +94,7 @@ const rgblight_segment_t PROGMEM sym_lights[] = RGBLIGHT_LAYER_SEGMENTS(
      {65, 1, COLOR_MAIN},
      {60, 1, COLOR_MAIN},
      {55, 1, COLOR_MAIN},
-     {26, 1, COLOR_ALT}     
+     {17, 1, COLOR_ALT}     
 );
 
 const rgblight_segment_t PROGMEM fn_lights[] = RGBLIGHT_LAYER_SEGMENTS(
