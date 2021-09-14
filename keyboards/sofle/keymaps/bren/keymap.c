cb7bf34f9ec04b95011c280114848172d5580813
Brenainn Moushall <bmoush@gmail.com>
2021-09-14 13:22:22 +1000

    Activate symbol layer with shift tap
    Too much thumb movement on left hand. Use tap dance to trigger
    the symbol layer when tapping the shift key, and regular
    shift when held. Still dialing in the tapping term; it's at
    a point where I can activate the sym layer and do a pair of
    braces, but I still need to be consciously patient when I want
    to type capitals.
    
    I've used the indicator lights to display the status of the tap
    function. Can't get the RHS indicator to work though, even though
    it works fine in an RGB layer.
    
    The old symbol layer key now activates just arrow keys. Might
    find other things to add to this layer. I could fit the arrows
    in the fn layer, but I want to keep that for meta/system actions,
    and avoid adding keys that get typed absent-mindedly (we have
    power down on that layer).

--- keyboards/sofle/keymaps/bren/keymap.c
+++ keyboards/sofle/keymaps/bren/keymap.c
@@ -4,22 +4,40 @@ enum layers {
     _QWERTY,
     _GAME,
     _SYM,
+    _ARR,
     _FN
 };
 
 enum custom_keycodes {
     KC_FN = MO(_FN),
     KC_SYM = MO(_SYM),
+    KC_ARROW = MO(_ARR),
     KC_GAME = TG(_GAME)
 };
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD
+} td_state_t;
+
+enum {
+    SYM_SHIFT
+ };
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+void sym_shift_finished(qk_tap_dance_state_t *state, void *user_data);
+void sym_shit_reset(qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT( \
       KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_INSERT, \
       KC_HOME,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_END, \
       KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOTE, \
-      KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
-                      KC_LALT, KC_LGUI, KC_SYM, KC_LSFT, KC_SPC,      KC_ENT,  KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
+      KC_LCTRL,  KC_Z,   KC_X,    KC_C,   KC_V,    KC_B, KC_MUTE,       XXXXXXX,KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DELETE, \
+                      KC_LALT, KC_LGUI, KC_ARROW, TD(SYM_SHIFT), KC_SPC,      KC_ENT,  KC_BSPC, KC_FN, KC_PGDOWN, KC_PGUP \
     ),
     [_GAME] = LAYOUT( \
       KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_INSERT, \
@@ -30,10 +48,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SYM] = LAYOUT( \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-      XXXXXXX, XXXXXXX, KC_RPRN, KC_RBRC, KC_RCBR,  XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-      XXXXXXX, KC_UNDS, KC_LPRN, KC_LBRC, KC_LCBR, KC_EQUAL,                             KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,\
-      KC_LCTRL, KC_MINUS, XXXXXXX, KC_BSLS, KC_PIPE, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-      	     KC_LALT, KC_LGUI, _______, KC_LSFT, KC_SPC,                            KC_ENT, KC_BSPC, XXXXXXX, KC_PGDOWN, KC_PGUP \
+      XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      XXXXXXX, KC_UNDS, KC_LBRC, KC_RBRC, KC_EQUAL, KC_PLUS,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      KC_LCTRL, KC_MINUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      	     KC_LALT, KC_LGUI, XXXXXXX, _______, KC_SPC,                            KC_ENT, KC_BSPC, XXXXXXX, KC_PGDOWN, KC_PGUP \
+    ),
+    [_ARR] = LAYOUT( \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX, \
+      KC_LCTRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+              KC_LALT, KC_LGUI, _______, KC_LSFT, KC_SPC,                       KC_ENT, KC_BSPC, XXXXXXX, KC_PGDOWN, KC_PGUP \
     ),
     [_FN] = LAYOUT( \
       KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                 KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
@@ -44,13 +69,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-
 #ifdef RGBLIGHT_ENABLE
-
 #define COLOR_MAIN 173, 255, 128 //midnight blue
 #define COLOR_ALT 11, 176, 255 // coral pink
 #define COLOR_WARN 14, 255, 255 // orange
 #define COLOR_BACKLIGHT 11, 176, 255
+
+void lhs_ind(uint8_t h, uint8_t s, uint8_t v) {
+    rgblight_sethsv_at(h, s, v, 0);
+}
+
+void rhs_ind(uint8_t h, uint8_t s, uint8_t v) {
+    rgblight_sethsv_at(h, s, v, 36);
+}
+
+void both_ind(uint8_t h, uint8_t s, uint8_t v) {
+    lhs_ind(h, s, v);
+    rhs_ind(h, s, v);
+}
+#endif
+
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+	else return TD_SINGLE_HOLD;
+    }
+    return TD_UNKNOWN;
+}
+
+static td_state_t sym_shift_state;
+
+void sym_shift_finished(qk_tap_dance_state_t *state, void *user_data) {
+    sym_shift_state = cur_dance(state);
+    switch (sym_shift_state) {
+        case TD_SINGLE_TAP:
+	    if (layer_state_is(_SYM)) {
+		both_ind(0, 0, 0);
+		layer_off(_SYM);
+	    } else {
+		both_ind(COLOR_ALT);
+		layer_on(_SYM);
+	    }
+	    break;
+	case TD_SINGLE_HOLD:
+	    both_ind(COLOR_MAIN);
+	    register_code(KC_LSFT);
+	    break;
+	default:
+	    break;
+    }
+}
+
+void sym_shift_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (sym_shift_state) {
+        case TD_SINGLE_TAP:
+	    if (layer_state_is(_SYM)) {
+		both_ind(0, 0, 0);
+	        layer_off(_SYM);
+	    }
+            break;
+	case TD_SINGLE_HOLD:
+            both_ind(0, 0, 0);
+	    unregister_code(KC_LSFT);
+	    break;
+	default:
+	    break;
+    }
+    sym_shift_state = TD_NONE;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [SYM_SHIFT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, sym_shift_finished, sym_shift_reset, 180)
+};
+
+#ifdef RGBLIGHT_ENABLE
 
 extern rgblight_config_t rgblight_config;
 
@@ -81,20 +173,12 @@ const rgblight_segment_t PROGMEM game_lights[] = RGBLIGHT_LAYER_SEGMENTS(
      {29, 1, COLOR_ALT}
 );
 
-const rgblight_segment_t PROGMEM sym_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-     LHS_BACKLIGHT,
-     RHS_BACKLIGHT,
-     {8, 1, COLOR_MAIN},
-     {14, 2, COLOR_MAIN},
-     {19, 2, COLOR_MAIN},
-     {23, 3, COLOR_MAIN},
-     {28, 3, COLOR_MAIN},
-     {34, 2, COLOR_MAIN},
+const rgblight_segment_t PROGMEM arr_lights[] = RGBLIGHT_LAYER_SEGMENTS(
      {70, 1, COLOR_MAIN},
      {65, 1, COLOR_MAIN},
      {60, 1, COLOR_MAIN},
      {55, 1, COLOR_MAIN},
-     {17, 1, COLOR_ALT}     
+     {17, 1, COLOR_ALT}
 );
 
 const rgblight_segment_t PROGMEM fn_lights[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -118,7 +202,7 @@ const rgblight_segment_t PROGMEM fn_lights[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     main_lights,
     game_lights,
-    sym_lights,
+    arr_lights,
     fn_lights
 );
 
@@ -128,9 +212,9 @@ const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 //}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
+    rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY) || layer_state_cmp(state, _SYM));
     rgblight_set_layer_state(1, layer_state_cmp(state, _GAME));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _SYM));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _ARR));
     rgblight_set_layer_state(3, layer_state_cmp(state, _FN));
     return state;
 }
