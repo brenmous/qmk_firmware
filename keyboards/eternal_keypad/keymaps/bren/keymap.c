a6888f31b001afc701ad75db2bd7dba9bd1e64b3
Bren Moushall <bmoush@gmail.com>
2022-07-21 20:14:48 +1000

    More tweaks

--- keyboards/eternal_keypad/keymaps/bren/keymap.c
+++ keyboards/eternal_keypad/keymaps/bren/keymap.c
@@ -18,6 +18,7 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
+    _ARR,
     _CFG
 };
 
@@ -27,11 +28,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, 
 		KC_VOLU, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H,
 		KC_MUTE, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,
-		KC_VOLD, KC_SLSH, MO(_CFG), KC_LALT, KC_SPC, KC_PLUS
+		KC_VOLD, MO(_ARR), MO(_CFG), KC_LALT, KC_SPC, KC_ENTER
+    ),
+    [_ARR] = LAYOUT(
+
+		                _______, KC_7, KC_8, KC_9, KC_0, _______, _______, 
+                                _______, _______, KC_UP, _______, _______, _______, _______,
+		_______,        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, 
+		_______,  _______, _______, _______, _______, _______, _______, _______, 
+		_______,        _______, _______, _______, _______, _______
     ),
     [_CFG] = LAYOUT(
-		                _______, _______, _______, _______, _______, _______, _______,
-		                _______, _______, _______, _______, _______, _______, _______, 
+		                _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
+		                _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
 		_______,        _______, _______, _______, _______, _______, _______, _______, 
 		QK_BOOTLOADER,  _______, _______, _______, _______, _______, _______, _______, 
 		_______,        _______, _______, _______, _______, _______
